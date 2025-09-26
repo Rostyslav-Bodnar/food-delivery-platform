0dc143c593cd31f5fac4f4bd64268907f35e834e
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-09-26 20:04:47 +0300

    UserService/update-API-project

---
+++ backend/UserService/DF.UserService.API/Program.cs
@@ -0,0 +1,48 @@
+﻿using DF.UserService.Application.Interfaces;
+using DF.UserService.Application.Services;
+using DF.UserService.Domain.Entities;
+using DF.UserService.Infrastructure.Data;
+using DF.UserService.Infrastructure.Messaging;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// MSSQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Identity
+builder.Services.AddIdentity<User, IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// JWT TODO: setup real JWT config
+builder.Services.AddAuthentication("Bearer").AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = false
+    };
+});
+
+// DI
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddSingleton<IMessageBroker, RabbitMqMessageBroker>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
