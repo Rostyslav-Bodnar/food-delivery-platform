b6e35693667aa6c0763225de4b5140ce95b0e852
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-09-26 22:27:50 +0300

    frontend/refactor

--- frontend/food-delivery-platform/src/App.jsx
+++ frontend/food-delivery-platform/src/App.jsx
@@ -1,6 +1,7 @@
 ﻿import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RegisterForm from "./components/RegisterForm";
+import LoginForm from "./components/LoginForm";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
                 <main>
                     <Routes>
                         <Route path="/register" element={<RegisterForm />} />
+                        <Route path="/login" element={<LoginForm />} />
                         <Route path="/" element={<h2 className="welcome">Welcome to Foodie Delivery 🍔</h2>} />
                     </Routes>
                 </main>
