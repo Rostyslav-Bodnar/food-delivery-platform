7dea684c047d8c9f99162d9310d8e855ba7ccb68
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-10-06 23:20:31 +0300

    user-service/ fix api route

--- frontend/food-delivery-platform/src/api/Profile.jsx
+++ frontend/food-delivery-platform/src/api/Profile.jsx
@@ -1,6 +1,6 @@
 ﻿import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "https://localhost:5001/api";
+const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const getProfileData = async (token) => {
     const response = await axios.get(`${API_BASE}/profile`, {
