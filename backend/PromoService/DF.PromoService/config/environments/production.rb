4d769b14ccc3cc51d912c93510c15de96cc6a796
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-10-16 00:58:25 +0300

    promo-service/ configure docker pipeline

--- backend/PromoService/DF.PromoService/config/environments/production.rb
+++ backend/PromoService/DF.PromoService/config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
-
+  config.hosts << "host.docker.internal"
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
