cd8a433a1132e95e412226f768eb5a0312a18f53
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-10-19 18:20:13 +0300

    menu-service/add projects

---
+++ backend/MenuService/DF.MenuService.Application/Repositories/DishRepository.cs
@@ -0,0 +1,47 @@
+using DF.MenuService.Application.Repositories.Interfaces;
+using DF.MenuService.Domain.Entities;
+using DF.MenuService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DF.MenuService.Application.Repositories;
+
+public class DishRepository(AppDbContext dbContext) : IDishRepository
+{
+
+    public async Task<Dish?> Get(Guid id)
+    {
+        return await dbContext.Dishes.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Dish?>> GetAll()
+    {
+        return await dbContext.Dishes.ToListAsync();
+    }
+
+    public async Task<Dish> Create(Dish entity)
+    {
+        await dbContext.Dishes.AddAsync(entity);
+        await dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<Dish> Update(Dish entity)
+    {
+        dbContext.Dishes.Update(entity);
+        await dbContext.SaveChangesAsync();
+        return entity;
+
+    }
+
+
+    public async Task<bool> Delete(Guid id)
+    {
+        var dish = await dbContext.Dishes.FindAsync(id);
+        if (dish == null)
+            return false;
+
+        dbContext.Dishes.Remove(dish);
+        await dbContext.SaveChangesAsync();
+        return true;
+    }
+}
