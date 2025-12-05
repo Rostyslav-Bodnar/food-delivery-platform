612cb4700a065c103e0bd9bd9d1d91f6d7c795ec
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-12-05 21:09:13 +0200

    order-service - service initialization

---
+++ backend/OrderService/DF.OrderService.API/Program.cs
@@ -0,0 +1,52 @@
+using DF.OrderService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173") // адреса фронтенду
+            .AllowAnyHeader()                     // дозволяємо всі заголовки
+            .AllowAnyMethod()                   // дозволяємо всі HTTP методи
+            .AllowCredentials();               // розкоментуй, якщо потрібні куки або авторизація
+    });
+});
+
+// Database connection (PostgreSQL)
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        o => o.UseNetTopologySuite()
+    ));
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); 
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ backend/OrderService/DF.OrderService.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,93 @@
+﻿using DF.OrderService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DF.OrderService.Infrastructure.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderedDish> OrderedDishes { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        // LOCATION
+        modelBuilder.Entity<Location>(entity =>
+        {
+            entity.ToTable("Locations");
+
+            entity.HasKey(l => l.Id);
+
+            entity.Property(l => l.FullAddress)
+                .HasMaxLength(300)
+                .IsRequired();
+
+            entity.Property(l => l.City)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            entity.Property(l => l.Street)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            entity.Property(l => l.House)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            entity.Property(l => l.GeoPoint)
+                .HasColumnType("geography (point)")
+                .IsRequired(false);
+
+        });
+
+        // ORDER
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.ToTable("Orders");
+
+            entity.HasKey(o => o.Id);
+
+            entity.Property(o => o.OrderDate)
+                .IsRequired();
+
+            entity.Property(o => o.TotalPrice)
+                .HasColumnType("decimal(10,2)")
+                .IsRequired();
+
+            entity.Property(o => o.OrderStatus)
+                .HasConversion<int>()
+                .IsRequired();
+
+            entity.HasOne(o => o.DeliverTo)
+                .WithMany()
+                .HasForeignKey(o => o.DeliverToId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            entity.HasOne(o => o.DeliverFrom)
+                .WithMany()
+                .HasForeignKey(o => o.DeliverFromId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
+
+        // ORDERED DISH
+        modelBuilder.Entity<OrderedDish>(entity =>
+        {
+            entity.ToTable("OrderedDishes");
+
+            entity.HasKey(od => od.Id);
+
+            entity.Property(od => od.OrderId)
+                .IsRequired();
+
+            entity.Property(od => od.DishId)
+                .IsRequired();
+
+            entity.HasOne(od => od.Order)
+                .WithMany()
+                .HasForeignKey(od => od.OrderId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+    }
+}
