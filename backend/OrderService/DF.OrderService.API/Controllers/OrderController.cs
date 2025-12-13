ddcf8b1a6c077c6d6afdeb2811cb64135c35c343
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-12-13 03:31:30 +0200

    order-service - add order controller

---
+++ backend/OrderService/DF.OrderService.API/Controllers/OrderController.cs
@@ -0,0 +1,59 @@
+﻿using DF.OrderService.Application.Services.Interfaces;
+using DF.OrderService.Contracts.Models.Requests;
+using DF.OrderService.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DF.OrderService.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class OrderController(IOrderService orderService) : ControllerBase
+{
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAll()
+    {
+        return Ok(await orderService.GetAllOrdersAsync());
+    }
+
+    [HttpGet("business")]
+    public async Task<IActionResult> GetByBusiness(Guid businessId)
+    {
+        return Ok(await orderService.GetAllByBusinessIdAsync(businessId));
+    }
+
+    [HttpGet("courier")]
+    public async Task<IActionResult> GetByCourier(Guid courierId)
+    {
+        return Ok(await orderService.GetAllByCourierIdAsync(courierId));
+    }
+
+    [HttpPatch("status")]
+    public async Task<IActionResult> ChangeStatus(Guid orderId, OrderStatus status)
+    {
+        return Ok(await orderService.ChangeOrderStatus(orderId, status));
+    }
+    
+    [HttpGet]
+    [Route("get-customer-orders")]
+    public async Task<IActionResult> GetCustomerOrders(Guid customerId)
+    {
+        var result = await orderService.GetAllByCustomerIdAsync(customerId);
+        return Ok(result);
+    }
+    
+    [HttpPost]
+    [Route("create-order")]
+    public async Task<IActionResult> CreateOrder([FromBody] CreateOrderRequest request)
+    {
+        var result = await orderService.CreateOrderAsync(request);
+        return Ok(result);
+    }
+    
+    [HttpPost]
+    [Route("create-orders")]
+    public async Task<IActionResult> CreateOrders([FromBody] List<CreateOrderRequest> request)
+    {
+        var result = await orderService.CreateOrdersAsync(request);
+        return Ok(result);
+    }
+}
