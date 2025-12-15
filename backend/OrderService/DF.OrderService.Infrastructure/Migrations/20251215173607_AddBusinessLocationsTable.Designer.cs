fa59755e8cad7717f9231a2a4ad2f576c0b578bb
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-12-15 19:36:46 +0200

    order-service - add business locations table

--- backend/OrderService/DF.OrderService.Infrastructure/Migrations/20251215173607_AddBusinessLocationsTable.Designer.cs
+++ backend/OrderService/DF.OrderService.Infrastructure/Migrations/20251215173607_AddBusinessLocationsTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DF.OrderService.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DF.OrderService.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251215173607_AddBusinessLocationsTable")]
+    partial class AddBusinessLocationsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
