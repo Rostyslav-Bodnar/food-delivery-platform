8601b82a4573618af98ebda6855d3c371cfaafce
rostik5720693@gmail.com <rostik5720693@gmail.com>
2025-12-16 22:08:10 +0200

    tracking-service - add dtos to contracts

---
+++ backend/Contracts/DF.Contracts/RPC/Requests/TrackingService/BusinessLocationRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace DF.Contracts.RPC.Requests.TrackingService;
+
+public record UpdateBusinessLocationRequest(
+    Guid  BusinessId,
+    Guid BusinessLocationId,
+    string FullAddress,
+    string City,
+    string Street,
+    string House
+);
+
+public record CreateBusinessLocationRequest(
+    Guid  BusinessId,
+    string FullAddress,
+    string City,
+    string Street,
+    string House
+);
---
+++ backend/Contracts/DF.Contracts/RPC/Responses/TrackingService/BusinessLocationResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace DF.Contracts.RPC.Responses.TrackingService;
+
+public record UpdateBusinessLocationResponse(
+    Guid  BusinessId,
+    Guid BusinessLocationId,
+    string FullAddress,
+    string City,
+    string Street,
+    string House
+    );
+    
+public record CreateBusinessLocationResponse(
+    Guid  BusinessId,
+    Guid BusinessLocationId,
+    string FullAddress,
+    string City,
+    string Street,
+    string House
+);
