7e431c9a5c0705f31679374dcbe68c8550249efd
rostik5720693@gmail.com <rostik5720693@gmail.com>
2026-03-20 23:34:24 +0200

    FDP-OrderService - add payment method to orders

---
+++ backend/OrderService/DF.OrderService.Infrastructure/Migrations/20260313194202_AddPaymentMethodToOrderEntity.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DF.OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentMethodToOrderEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentMethod",
+                table: "Orders",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Orders");
+        }
+    }
+}
