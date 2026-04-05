65293aa3ad7e4924b93f223ce2035542cc2dbc76
rostik5720693@gmail.com <rostik5720693@gmail.com>
2026-04-05 17:53:43 +0300

    #FDP-Contracts - add business location response

--- backend/Contracts/DF.Contracts/RPC/Responses/TrackingService/BusinessLocationResponse.cs
+++ backend/Contracts/DF.Contracts/RPC/Responses/TrackingService/BusinessLocationResponse.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DF.Contracts.RPC.Responses.TrackingService;
 
@@ -18,4 +19,20 @@ public record CreateBusinessLocationResponse(
     string City,
     string Street,
     string House
+);
+
+public record GetBusinessLocationsResponse (
+    IEnumerable<GetBusinessLocationResponse> BusinessLocations
+        );
+
+public record GetBusinessLocationResponse(
+    Guid BusinessId,
+    Guid BusinessLocationId,
+    string FullAddress,
+    string City,
+    string Street,
+    string House,
+    double Latitude,
+    double Longitude
+    
 );
