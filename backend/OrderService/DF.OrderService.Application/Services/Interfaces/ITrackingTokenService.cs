b65d6993bbf966ea40f516ab2269e06156871282
rostik5720693@gmail.com <rostik5720693@gmail.com>
2026-04-15 23:09:07 +0300

    #FDP-OrderService - add tracking token

---
+++ backend/OrderService/DF.OrderService.Application/Services/Interfaces/ITrackingTokenService.cs
@@ -0,0 +1,13 @@
+﻿using DF.OrderService.Contracts.Models.Responses;
+
+namespace DF.OrderService.Application.Services.Interfaces;
+
+public interface ITrackingTokenService
+{
+    TrackingAccessTokenResponse CreateTrackingToken(
+        Guid subjectId,
+        string role,
+        Guid orderId,
+        IReadOnlyCollection<string> scopes,
+        TimeSpan lifetime);
+}
---
+++ backend/OrderService/DF.OrderService.Contracts/Models/Responses/TrackingAccessTokenResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace DF.OrderService.Contracts.Models.Responses;
+
+public record TrackingAccessTokenResponse
+(
+    string Token,
+    DateTime ExpiresAt
+    );
