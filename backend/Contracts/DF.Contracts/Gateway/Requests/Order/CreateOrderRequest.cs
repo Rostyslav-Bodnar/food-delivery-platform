62c6e6b9adbe4c4915e1ed5cb00959b8dabf02b2
rostyslav.bodnar-ip224@nung.edu.ua <rostyslav.bodnar-ip224@nung.edu.ua>
2026-05-10 22:44:52 +0300

    #64 - add response/request dtos to DF.Contracts

---
+++ backend/Contracts/DF.Contracts/Gateway/Requests/Order/CreateOrderRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using DF.Contracts.Enums;
+
+namespace DF.Contracts.Gateway.Requests.Order;
+
+public record CreateOrderRequest(
+    Guid BusinessId,
+    Guid OrderedBy,
+    DateTime OrderDate,
+    decimal TotalPrice,
+    Guid? DeliveredBy,
+    CreateLocationRequest DeliverTo,
+    CreateLocationRequest DeliverFrom,
+    PaymentMethod PaymentMethod,
+    List<CreateOrderDishRequest> Dishes
+);
---
+++ backend/Contracts/DF.Contracts/Gateway/Responses/Order/LocationResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace DF.Contracts.Gateway.Responses.Order;
+
+public record LocationResponse(
+    string FullAddress,
+    string City,
+    string Street,
+    string House,
+    string Latitude,
+    string Longitude
+);
