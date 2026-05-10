4971f8c1f3b606a6b8423a1ad0568a779b2b79a4
rostyslav.bodnar-ip224@nung.edu.ua <rostyslav.bodnar-ip224@nung.edu.ua>
2026-05-10 23:00:08 +0300

    #64 - add dtos to frontend

---
+++ frontend/food-delivery-platform/src/models/requests/order/CreateOrderRequest.ts
@@ -0,0 +1,13 @@
+﻿import {CreateLocationRequest} from "./CreateLocationRequest";
+
+export interface CreateOrderDishRequest {
+    businessId : string
+    orderedBy : string
+    orderDate : string
+    totalPrice : number
+    deliveredBy? : string | null
+    deliverTo : CreateLocationRequest
+    deliverFrom : CreateLocationRequest
+    paymentMethod : number
+    dishes : CreateOrderDishRequest[]
+}
